9c488e752bd120d5796ec7e66998dfb61939e0a4
n-ce <abrocks1120@gmai.com>
2021-02-23 21:34:23 +0530

    pwa

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const staticLoader = "Binaurma"
+const assets = [
+  "/",
+  "index.html",
+  "192px_icon.png",
+  "512px_icon.png",
+  "script.js",
+  "style.css",
+]
+
+self.addEventListener("install", installEvent => {
+	installEvent.waitUntil(
+		caches.open(staticLoader).then(cache => {
+			cache.addAll(assets)
+		})
+	)
+})
+self.addEventListener("fetch", fetchEvent => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request)
+		})
+	)
+})
