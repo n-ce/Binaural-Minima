9fbf6aaf1ccec6e354061baf4f56c3e074386538
n-ce <abrocks1120@gmail.com>
2021-02-24 18:53:12 +0530

    attempt 4

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 var ctx = new AudioContext();
 var osc = ctx.createOscillator();
 var gain = ctx.createGain();
-
-gain.gain.value=0;
-
+function start(){
 osc.connect(gain);
 gain.connect(ctx.destination);
-
+gain.gain.value=0;
 osc.start(0);
+}
 var f = document.querySelector("input");
 f.addEventListener("input",(event)=>{
-console.log(event.target.value);
-osc.frequency.value=event.target.value; });
-function play() {
+osc.frequency.value=event.target.value;
+document.querySelector("h2").innerHTML = f.value + "hz"});
+function play(){
   gain.gain.value = 1;
-  document.querySelector("h2").innerHTML = f.value + "hz";
 }
 function stop(){
   gain.gain.value = 0;
